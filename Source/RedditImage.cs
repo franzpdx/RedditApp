5167d46aef6481521012ec88a3147aadd014a6bb
franzpdx <thomas.allen.franz@gmail.com>
2021-09-18 18:37:34 -0700

    Source & Build folders, class files
    Inside the project: Now we have separate files for the RedditImage class and SubredditImages classes
    Github: Renamed App folder to Source, because it contains the source code. Added Build folder to contain the executable. Removed extraneous files that were in the debug/release folders.

---
+++ Source/RedditImage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace RedditApp
+{
+    // Struct:      RedditImage
+    // Purpose:     Contains an image and metadata associated with that image
+    public struct RedditImage
+    {
+        public BitmapImage Image { get; set; }
+        public string URL { get; set; }
+        public string FileName { get; set; }
+        public bool IsNSFW { get; set; }
+    }
+}
---
+++ Source/SubredditImages.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace RedditApp
+{
+
+    // Class:       SubredditImages
+    // Purpose:     Represents a collection of images pulled down from a Subreddit
+    [Serializable]
+    public class SubredditImages
+    {
+        [NonSerialized]
+        public List<RedditImage> ImageList;
+
+        public List<RedditImage> Images { get { return ImageList; } set { ImageList = value; } }
+
+        public string SubredditName { get; set; }
+
+        public bool Selected { get; set; }
+
+        // Label is used in our databinding to display the names of the subreddits along with the image counts
+        public string Label
+        {
+            get
+            {
+                string SubredditLabel = "";
+                string Count = "0";
+
+                int Hidden = 0;
+                if (Images != null)
+                {
+                    Count = Images.Count.ToString();
+
+                    foreach (RedditImage image in Images)
+                    {
+                        if (image.IsNSFW)
+                            Hidden++;
+                    }
+                }
+
+                if (Hidden == 0)
+                    SubredditLabel = SubredditName + " [" + Count + "]";
+                else
+                    SubredditLabel = SubredditName + " [" + Count + " / " + Hidden.ToString() + "]";
+
+                return SubredditLabel;
+            }
+            set { }
+        }
+
+        // Constructor with SubredditName setter (and empty image List)
+        public SubredditImages(string Name)
+        {
+            SubredditName = Name;
+            Images = new List<RedditImage>();
+            Selected = true;
+        }
+
+        // Constructor with SubredditName and Selected setters (and empty image List)
+        public SubredditImages(string Name, bool IsSelected)
+        {
+            SubredditName = Name;
+            Images = new List<RedditImage>();
+            Selected = IsSelected;
+        }
+
+        // Constructor with SubredditName and Image List setters
+        public SubredditImages(string Name, List<RedditImage> ImageList)
+        {
+            SubredditName = Name;
+            Images = ImageList;
+            Selected = true;
+        }
+    }
+}
